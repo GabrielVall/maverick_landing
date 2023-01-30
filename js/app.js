4c637dd0f972a0d046385c7c4bb7837ecb21043b
Gabriel Vallejo <gabrielvallejo2000@gmail.com>
2023-01-30 14:21:45 -0600

    cambio ref

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(function() {
-    $(document).on("click","[ref]", function(){
+    $(document).on("click","[data-ref]", function(){
         let url = $(this).data('ref');
         window.open(
             url,
